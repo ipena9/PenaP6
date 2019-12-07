7f9f2e2a7b2a32054f8f56f3b5ee0ff3fab19fca
ipena9 <54920509+ipena9@users.noreply.github.com>
2019-12-06 23:36:57 -0700

    Add files via upload

---
+++ PenaP6/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(PenaP6.Startup))]
+
+namespace PenaP6
+{
+    public class Startup
+    {
+        //Used https://docs.microsoft.com/en-us/aspnet/signalr/overview/getting-started/tutorial-getting-started-with-signalr
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
